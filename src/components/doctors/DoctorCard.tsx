882e2ceb842a9f0210a0c64b5f86dc5d5998fb9d
Jonathan Alexis Bello Lopez <jonathan.bellofg@gmail.com>
2025-10-30 04:00:29 -0600

    add: hover de card de doctor

--- src/components/doctors/DoctorCard.tsx
+++ src/components/doctors/DoctorCard.tsx
@@ -15,7 +15,7 @@ export default function DoctorCard({ doctor, onSelect }: DoctorCardProps) {
     "focus:outline-none focus-visible:ring focus-visible:ring-primary-blue-300 focus-visible:ring-offset-2 focus-visible:ring-offset-white";
 
   const content = (
-    <article className="flex max-w-40 flex-col items-center lg:max-w-52">
+    <article className="flex max-w-40 flex-col items-center lg:max-w-52 hover:cursor-pointer group">
       <div className="relative mb-4 flex size-40 items-center justify-center overflow-hidden rounded-full lg:size-48">
         <img
           src={image.src}
@@ -24,7 +24,7 @@ export default function DoctorCard({ doctor, onSelect }: DoctorCardProps) {
           className="h-full w-full rounded-full object-cover"
         />
       </div>
-      <p className="text-center text-base font-normal text-primary-grey lg:text-lg">
+      <p className="text-center text-base font-normal text-primary-grey lg:text-lg group-hover:underline">
         {name}
       </p>
     </article>
